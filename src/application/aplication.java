100dadc610ee3afdf4ecd1244517f678a37085d9
DESKTOP-V5KCN01\admin <anhltph24057@fpt.edu.vn>
2022-11-11 22:28:00 +0700

    abc

--- src/application/aplication.java
+++ src/application/aplication.java
@@ -12,7 +12,7 @@ public class aplication {
     public static void main(String[] args) {
         System.out.println("hsjfhasjkshasdjah");
         System.out.println("hi");
-        System.out.println("toan");
+        System.out.println("tuuuuuu");
     }
     
 }
