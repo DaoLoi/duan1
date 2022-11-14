44275c82920c5f78b0d43c60ead9996534d2184c
DESKTOP-V5KCN01\admin <anhltph24057@fpt.edu.vn>
2022-11-14 10:33:41 +0700

    .

---
+++ src/model/KhachHang.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author admin
+ */
+public class KhachHang {
+    private String idKH, ten, sdt, diaChi, ngayLap, ngaySua;
+    private int trangThai;
+
+    public KhachHang() {
+    }
+
+    public KhachHang(String idKH, String ten, String sdt, String diaChi, String ngayLap, String ngaySua, int trangThai) {
+        this.idKH = idKH;
+        this.ten = ten;
+        this.sdt = sdt;
+        this.diaChi = diaChi;
+        this.ngayLap = ngayLap;
+        this.ngaySua = ngaySua;
+        this.trangThai = trangThai;
+    }
+
+    public String getIdKH() {
+        return idKH;
+    }
+
+    public void setIdKH(String idKH) {
+        this.idKH = idKH;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getNgayLap() {
+        return ngayLap;
+    }
+
+    public void setNgayLap(String ngayLap) {
+        this.ngayLap = ngayLap;
+    }
+
+    public String getNgaySua() {
+        return ngaySua;
+    }
+
+    public void setNgaySua(String ngaySua) {
+        this.ngaySua = ngaySua;
+    }
+
+    public int getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(int trangThai) {
+        this.trangThai = trangThai;
+    }
+    
+}
