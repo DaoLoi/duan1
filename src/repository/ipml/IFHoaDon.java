0786088c6a08c202628462716fca410e3c38c36a
LAPTOP-U7MQ6KHT\ASUS <hoppdph23992@fpt.edu.vn>
2022-11-14 10:41:48 +0700

    sua lai loi

--- src/repository/ipml/IFHoaDon.java
+++ src/repository/ipml/IFHoaDon.java
@@ -2,7 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package service.ipml;
+package repository.ipml;
+
 
 import model.HoaDon;
 import java.util.List;
