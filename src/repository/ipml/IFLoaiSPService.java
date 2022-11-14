2763b5812058a9f1d04d40c5620484a4509d601d
DESKTOP-QI00RH3\TDC <anhvtph24041@fpt.edu.vn>
2022-11-14 10:58:26 +0700

    abc

---
+++ src/repository/ipml/IFLoaiSPService.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package service.ipml;
+
+import model.LoaiSanPham;
+import java.util.List;
+
+/**
+ *
+ * @author VU TUAN
+ */
+public interface IFLoaiSPService {
+    List<LoaiSanPham> getLSP();
+    public void add(LoaiSanPham list);
+    
+}
