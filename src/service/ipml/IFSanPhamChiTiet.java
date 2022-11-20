ee5b31dd1bf1adb4f348e5317cb23b066cec26c2
LAPTOP-U7MQ6KHT\ASUS <hoppdph23992@fpt.edu.vn>
2022-11-21 03:39:36 +0700

    Chi tiết sản phẩm hay sản phẩm chi tiết ?

---
+++ src/service/ipml/IFSanPhamChiTiet.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package service.ipml;
+
+
+import java.util.List;
+import model.sanPhamChiTietModel;
+
+/**
+ *
+ * @author ASUS
+ */
+public interface IFSanPhamChiTiet {
+    List<sanPhamChiTietModel> getAll();
+    
+    public boolean add(sanPhamChiTietModel spCT);
+    
+    public boolean update(sanPhamChiTietModel spCT, String idCTsp);
+    
+    public boolean delete(String idCTsp);
+}
